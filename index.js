02cc4dbb07664a3d61a5c6b35701ecab13b70d6a
Stas Kyaune <kyaune@gmail.com>
2019-10-31 00:04:20 +0300

    222

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// JSX
-ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
-
-var myNewP = document.createElement("p")
-myNewP.innerHTML = "This is a paragraph."
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
