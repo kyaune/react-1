4b384a1a3e9e4343139a2ff352699251e28deae7
Stas Kyaune <kyaune@gmail.com>
2019-11-01 00:29:00 +0300

    added todolist, todoitem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
+  const todos = [
+    { id: 1, completed: false, title: "Купить кушоц" },
+    { id: 2, completed: false, title: "Купить 2" },
+    { id: 3, completed: false, title: "Купить 3" }
+  ];
   return (
     <div className="wrapper">
       <h1>React tutorial</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
