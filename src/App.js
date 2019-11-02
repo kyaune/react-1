fb36b06bee7cc11a7a8d23b777be04c2b06d8e16
Stas Kyaune <kyaune@gmail.com>
2019-11-02 14:01:11 +0300

    added NoTodos and AddTodos

--- src/App.js
+++ src/App.js
@@ -27,7 +27,11 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        <TodoList todos={todos} onToggle={toggleTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>No todos</p>
+        )}
       </div>
     </Context.Provider>
   );
