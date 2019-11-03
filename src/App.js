0163ae59d2219e7ee68751aa9350760912cf4842
Stas Kyaune <kyaune@gmail.com>
2019-11-03 16:36:20 +0300

    added Addtodo usability

--- src/App.js
+++ src/App.js
@@ -24,11 +24,24 @@ function App() {
   function removeTodo(id) {
     setTodos(todos.filter(todo => todo.id !== id));
   }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    );
+  }
+
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        <AddTodo />
+        <AddTodo onCreate={addTodo} />
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
         ) : (
--- src/todo/AddTodo.js
+++ src/todo/AddTodo.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import propTypes from "prop-types";
 
-function AddTodo() {
+function AddTodo({ onCreate }) {
+  const [value, setValue] = useState("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    if (value.trim()) {
+      onCreate(value);
+      setValue(" ");
+    }
+  }
   return (
-    <form>
-      <input />
+    <form style={{ marginBottom: "1rem" }} onSubmit={submitHandler}>
+      <input value={value} onChange={event => setValue(event.target.value)} />
       <button type="submit">Add Todo</button>
     </form>
   );
 }
+
+AddTodo.propTypes = {
+  onCreate: propTypes.func.isRequired
+};
 
 export default AddTodo;
